5490c6b93ca841c732a29288328b0ab43395c6da
NandoNunez <hnandon@hotmail.com>
2023-08-04 10:30:11 -0500

    Nuevo Cambio

--- AppConsola/Program.cs
+++ AppConsola/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Vamos a hacer unos cambios");
-Console.WriteLine("Hola soy Hernando");
-Console.WriteLine("Hola soy Anderson");
+Console.WriteLine("Hola soy Hernando NUñez");
+Console.WriteLine("Hola soy Anderson SanJuan");
